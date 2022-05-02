ca53cef35d37b4fab0bb9acc8af0718cae5adaa4
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-02 13:31:17 +0200

    feat(backend): add JWT

---
+++ backend/src/main/java/io/github/xpakx/micro2/user/UserService.java
@@ -0,0 +1,25 @@
+package io.github.xpakx.micro2.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository user;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserAccount userAccount = user.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("No user with username " + username));
+        return new User(userAccount.getUsername(), userAccount.getPassword(), userAccount.getRoles());
+    }
+
+    public UserDetails userAccountToUserDetails(UserAccount userAccount) {
+        return new User(userAccount.getUsername(), userAccount.getPassword(), userAccount.getRoles());
+    }
+}
