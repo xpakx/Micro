552b65f1f146427692d66f45756734bd466a7e6e
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-02 13:27:33 +0200

    chore(backend): add user entities

---
+++ backend/src/main/java/io/github/xpakx/micro2/user/UserRepository.java
@@ -0,0 +1,11 @@
+package io.github.xpakx.micro2.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByUsername(String username);
+}
