a5c7de6367d5f9cdd6b39138be1eb6bc264fee7d
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-02 13:20:36 +0200

    chore(backend): init

---
+++ backend/src/test/java/io/github/xpakx/micro2/MicroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.micro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
