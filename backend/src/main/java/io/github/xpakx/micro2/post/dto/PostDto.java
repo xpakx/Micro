1717841bdd80358f2a28cd4711095770a700f27f
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-05 11:57:03 +0200

    feat(backend): add creation date to PostDto

--- backend/src/main/java/io/github/xpakx/micro2/post/dto/PostDto.java
+++ backend/src/main/java/io/github/xpakx/micro2/post/dto/PostDto.java
@@ -4,18 +4,22 @@ import io.github.xpakx.micro2.post.Post;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 public class PostDto {
     private Long id;
     private String message;
     private String username;
+    private LocalDateTime createdAt;
 
     public static PostDto fromPost(Post post) {
         PostDto transformed = new PostDto();
         transformed.setId(post.getId());
         transformed.setMessage(post.getContent());
         transformed.setUsername(post.getUser().getUsername());
+        transformed.setCreatedAt(post.getCreatedAt());
         return  transformed;
     }
 }
