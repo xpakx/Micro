9b6a15a62b2d9371e333629d6200f2c8185c812c
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-06 11:56:17 +0200

    feat(frontend): add register logic

--- frontend/src/app/authentication/register-form/register-form.component.ts
+++ frontend/src/app/authentication/register-form/register-form.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
+import { Token } from '../dto/token';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.css']
 })
 export class RegisterFormComponent implements OnInit {
+  form: FormGroup;
+  public invalid: boolean = false;
+  public message: string = '';
 
-  constructor() { }
+  constructor(private service: AuthenticationService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      passwordRe: ['', Validators.required]
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  authenticate(): void {
+    if(this.form.valid) {
+      this.invalid = false;
+      this.service.register({
+        username: this.form.controls['username'].value,
+        password: this.form.controls['password'].value,
+        passwordRe: this.form.controls['passwordRe'].value,
+      }).subscribe(
+        (response: Token) => {
+          localStorage.setItem("token", response.token);
+          localStorage.setItem("username", response.username);
+        },
+        (error: HttpErrorResponse) => {
+          this.message = error.error.message;
+          this.invalid = true;
+        }
+      )
+    } else {
+      this.message = "Fields cannot be empty!";
+      this.invalid = true;
+    }
+  }
 }
