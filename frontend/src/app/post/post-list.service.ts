a12cf128811f97e152e6171cd55852a3d4eb5a35
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-07 14:12:12 +0200

    fix(frontend): use custom Page class instead of normal list

--- frontend/src/app/post/post-list.service.ts
+++ frontend/src/app/post/post-list.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Page } from '../common/dto/page';
 import { PostDetails } from './dto/post-details';
 
 @Injectable({
@@ -12,11 +13,11 @@ export class PostListService {
 
   constructor(private http: HttpClient) { }
 
-  public getPosts(page?: number | undefined):  Observable<PostDetails[]> {
-    return this.http.get<PostDetails[]>(`${this.apiServerUrl}/posts${page ? '/'+page : ''}`);
+  public getPosts(page?: number | undefined):  Observable<Page<PostDetails>> {
+    return this.http.get<Page<PostDetails>>(`${this.apiServerUrl}/posts${page ? '/'+page : ''}`);
   }
 
-  public getUserPosts(username: String, page?: number | undefined):  Observable<PostDetails[]> {
-    return this.http.get<PostDetails[]>(`${this.apiServerUrl}/user/${username}/posts${page ? '/'+page : ''}`);
+  public getUserPosts(username: String, page?: number | undefined):  Observable<Page<PostDetails>> {
+    return this.http.get<Page<PostDetails>>(`${this.apiServerUrl}/user/${username}/posts${page ? '/'+page : ''}`);
   }
 }
