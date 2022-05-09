f257bf8adcf6c4cc658e887c35bd097a28c52133
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-09 14:23:30 +0200

    feat(frontend/ui): add icons and buttons in post component

--- frontend/src/app/post/post/post.component.ts
+++ frontend/src/app/post/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faCheckCircle, faPaperclip, faPaperPlane, faPlus, faSmile, faStar } from '@fortawesome/free-solid-svg-icons';
 import { PostDetails } from '../dto/post-details';
 
 @Component({
@@ -8,6 +9,12 @@ import { PostDetails } from '../dto/post-details';
 })
 export class PostComponent implements OnInit {
   @Input('post') post!: PostDetails;
+  faCheck = faCheckCircle;
+  faPlus = faPlus;
+  faStar = faStar;
+  faSmile = faSmile;
+  faAttach = faPaperclip;
+  faSend = faPaperPlane;
 
   constructor() { }
 
