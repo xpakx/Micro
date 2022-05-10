9d626b7c2052824ea1390263412fac93ebff52ab
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-10 10:10:57 +0200

    feat(frontend): add general post form logic

--- frontend/src/app/post/post-form/post-form.component.ts
+++ frontend/src/app/post/post-form/post-form.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faPaperclip, faPaperPlane, faSmile } from '@fortawesome/free-solid-svg-icons';
+import { UpdatedPost } from '../dto/updated-post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-form',
@@ -7,13 +11,37 @@ import { faPaperclip, faPaperPlane, faSmile } from '@fortawesome/free-solid-svg-
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  form: FormGroup;
+  message: String = '';
+  invalid: boolean = false;
   faSmile = faSmile;
   faAttach = faPaperclip;
   faSend = faPaperPlane;
 
-  constructor() { }
+  constructor(private service: PostService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+    content: ['', Validators.required]
+  }); }
 
   ngOnInit(): void {
   }
 
+  sendPost(): void {
+    if(this.form.valid) {
+      this.service.newPost({message: this.form.controls['content'].value})
+      .subscribe({
+        next: (response: UpdatedPost) => this.refresh(response),
+        error: (error: HttpErrorResponse) => this.showError(error)
+      });
+    }
+  }
+
+  showError(error: HttpErrorResponse): void {
+    this.message = error.error.message;
+    this.invalid = true;
+  }
+
+  refresh(response: UpdatedPost): void {
+    
+  }
 }
