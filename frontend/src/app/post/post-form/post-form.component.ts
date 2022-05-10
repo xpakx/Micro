a26076e978673f19092b3317dc8afca1f5d2cb3b
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-10 10:03:24 +0200

    feat(frontend/ui): style post form

--- frontend/src/app/post/post-form/post-form.component.ts
+++ frontend/src/app/post/post-form/post-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPaperclip, faPaperPlane, faSmile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-post-form',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent implements OnInit {
+  faSmile = faSmile;
+  faAttach = faPaperclip;
+  faSend = faPaperPlane;
 
   constructor() { }
 
