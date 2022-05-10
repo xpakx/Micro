7d84a3b67cf67e8d4d927e399932dc48ccf9416f
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-10 10:23:30 +0200

    chore(frontend): create entity for comment

--- backend/src/main/java/io/github/xpakx/micro2/comment/Comment.java
+++ backend/src/main/java/io/github/xpakx/micro2/comment/Comment.java
@@ -1,29 +1,31 @@
-package io.github.xpakx.micro2.post;
+package io.github.xpakx.micro2.comment;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import io.github.xpakx.micro2.comment.Comment;
+import io.github.xpakx.micro2.post.Post;
 import io.github.xpakx.micro2.user.UserAccount;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
-public class Post {
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
     @Column(nullable = false, length = 20000)
     @NotEmpty
-    @Size(max= 20000, message = "Post can't be longer than 20.000 characters!")
+    @Size(max= 20000, message = "Comment can't be longer than 20.000 characters!")
     String content;
 
     @ManyToOne
@@ -34,6 +36,7 @@ public class Post {
     boolean edited;
 
     @JsonIgnore
-    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.MERGE})
-    private List<Comment> comments;
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
