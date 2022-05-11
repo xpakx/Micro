cb1cfbc2429a1fa9f33e071fa53c5a375412e116
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-11 12:16:19 +0200

    feat(backend): add endpoints to gettin comments for given post

---
+++ backend/src/main/java/io/github/xpakx/micro2/comment/CommentPublicViewController.java
@@ -0,0 +1,33 @@
+package io.github.xpakx.micro2.comment;
+
+import io.github.xpakx.micro2.comment.dto.CommentDetails;
+import io.github.xpakx.micro2.post.dto.PostDetails;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CommentPublicViewController {
+    private final CommentService service;
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<Page<CommentDetails>> getAllPosts(@PathVariable Long postId)
+    {
+        return new ResponseEntity<>(
+                service.getCommentsForPost(0, postId), HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/posts/{postId}/comments/{page}")
+    public ResponseEntity<Page<CommentDetails>> getAllPosts(@PathVariable Integer page, @PathVariable Long postId)
+    {
+        return new ResponseEntity<>(
+                service.getCommentsForPost(page, postId), HttpStatus.OK
+        );
+    }
+}
