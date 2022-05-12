4f5674d32ab908da8e204252f3045fb99e4a573c
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-12 12:32:02 +0200

    feat(frontend): add service methods for comment list operations

---
+++ frontend/src/app/comment/comment-list.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Page } from '../common/dto/page';
+import { CommentDetails } from './dto/comment-details';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentListService {
+  private apiServerUrl = environment.apiServerUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getComments(postId: number, page?: number | undefined):  Observable<Page<CommentDetails>> {
+    return this.http.get<Page<CommentDetails>>(`${this.apiServerUrl}/posts/${postId}/comments${page ? '/'+page : ''}`);
+  }
+}
