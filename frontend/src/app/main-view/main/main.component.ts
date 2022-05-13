be9ce713d38319432a39e9be990f59b634220661
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-13 10:28:14 +0200

    chore(frontend): delete code with test post

--- frontend/src/app/main-view/main/main.component.ts
+++ frontend/src/app/main-view/main/main.component.ts
@@ -17,11 +17,10 @@ export class MainComponent implements OnInit {
   constructor(private postService: PostListService) { }
 
   ngOnInit(): void {
-    /*this.postService.getPosts().subscribe({
+    this.postService.getPosts().subscribe({
       next: (response: Page<PostDetails>) => this.updateList(response),
       error: (error: HttpErrorResponse) => this.showError(error)
-    });*/
-    this.test();
+    });
   }
 
   showError(error: HttpErrorResponse): void {
@@ -32,35 +31,5 @@ export class MainComponent implements OnInit {
   updateList(response: Page<PostDetails>): void {
     this.postList = response;
     this.errorOccured = false;
-  }
-
-  test(): void {
-    this.postList = {
-      content: [{id: 0, content: "post", edited: false, createdAt: new Date(), user: {username: "Test", gender: "male", confirmed: true, avatarUrl: ""} }],
-      pageable: {
-          sort: {
-              sorted: true,
-              unsorted: true,
-              empty: false
-          },
-          offset: 1,
-          pageNumber: 1,
-          pageSize: 1,
-          paged: true,
-          unpaged: true
-      },
-      totalPages: 1,
-      last: true,
-      number: 1,
-      sort: {
-          sorted: true,
-          unsorted: true,
-          empty: false
-      },
-      size: 1,
-      numberOfElements: 1,
-      first: true,
-      empty: false
-  }
   }
 }
