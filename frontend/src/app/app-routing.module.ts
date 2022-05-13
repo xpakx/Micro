8f13ea0df8f3ae071056b596b536ba577830825f
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-13 10:02:13 +0200

    feat(frontend): add route for register and login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './authentication/login-form/login-form.component';
+import { RegisterFormComponent } from './authentication/register-form/register-form.component';
 import { MainComponent } from './main-view/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent }
+  { path: '', component: MainComponent },
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'login', component: LoginFormComponent }
 ];
 
 @NgModule({
