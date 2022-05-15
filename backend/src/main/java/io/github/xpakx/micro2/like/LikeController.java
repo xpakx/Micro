311689a115272de3f87c6bd63e312a791f3fb5da
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-15 11:09:33 +0200

    feat(backend): add endpoints for liking and unliking posts

---
+++ backend/src/main/java/io/github/xpakx/micro2/like/LikeController.java
@@ -0,0 +1,34 @@
+package io.github.xpakx.micro2.like;
+
+import io.github.xpakx.micro2.like.dto.LikeRequest;
+import io.github.xpakx.micro2.post.dto.PostDto;
+import io.github.xpakx.micro2.post.dto.PostRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user/{username}")
+public class LikeController {
+    private final LikeService service;
+
+    @PostMapping("/posts/{postId}/like")
+    @PreAuthorize("#username == authentication.principal.username")
+    public ResponseEntity<Like> likePost(@RequestBody LikeRequest request, @PathVariable String username, @PathVariable Long postId) {
+        return new ResponseEntity<>(
+                service.likePost(request, postId, username),
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping("/posts/{postId}/like")
+    @PreAuthorize("#username == authentication.principal.username")
+    public ResponseEntity<?> unlikePost(@PathVariable String username, @PathVariable Long postId) {
+        service.unlikePost(postId, username);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
+
