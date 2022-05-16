d6b55bfe4e12aaba89b6f8b57fa8630a393e1013
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-16 13:45:08 +0200

    chore(backend): delete unnecessary imports

--- backend/src/main/java/io/github/xpakx/micro2/security/WebSecurityConfiguration.java
+++ backend/src/main/java/io/github/xpakx/micro2/security/WebSecurityConfiguration.java
@@ -1,8 +1,6 @@
 package io.github.xpakx.micro2.security;
 
 import io.github.xpakx.micro2.user.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
