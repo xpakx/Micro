efa1446c6fe00f4bca5b5c3e8cd9673e32875220
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-17 14:17:14 +0200

    fix(backend): fix data format in CommentDetails

--- backend/src/main/java/io/github/xpakx/micro2/comment/dto/CommentDetails.java
+++ backend/src/main/java/io/github/xpakx/micro2/comment/dto/CommentDetails.java
@@ -1,5 +1,8 @@
 package io.github.xpakx.micro2.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import io.github.xpakx.micro2.user.dto.UserMin;
 
 import java.time.LocalDateTime;
@@ -8,6 +11,9 @@ public interface CommentDetails {
     Long getId();
     String getContent();
     UserMin getUser();
+    
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss")
     LocalDateTime getCreatedAt();
     boolean isEdited();
     Integer getLikeCount();
