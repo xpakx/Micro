3656034dcc0b3cb917a0c7d710dc92955ac93aae
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-19 15:09:29 +0200

    feat(backend): don't show content for deleted comments

--- backend/src/main/java/io/github/xpakx/micro2/comment/dto/CommentDetails.java
+++ backend/src/main/java/io/github/xpakx/micro2/comment/dto/CommentDetails.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import io.github.xpakx.micro2.user.dto.UserMin;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDateTime;
 
 public interface CommentDetails {
     Long getId();
+
+    @Value("#{target.deletedByUser ? '[comment deleted by author]' : target.content}")
     String getContent();
     UserMin getUser();
 
@@ -16,6 +19,7 @@ public interface CommentDetails {
     @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss")
     LocalDateTime getCreatedAt();
     boolean isEdited();
+    boolean isDeletedByUser();
     Integer getLikeCount();
     Integer getDislikeCount();
 }
