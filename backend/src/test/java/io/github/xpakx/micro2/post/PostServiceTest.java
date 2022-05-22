fdf04b76306b70cb5dd3f185a7c4ba9ed52849a4
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-22 11:46:37 +0200

    test(backend): add test for updating post

--- backend/src/test/java/io/github/xpakx/micro2/post/PostServiceTest.java
+++ backend/src/test/java/io/github/xpakx/micro2/post/PostServiceTest.java
@@ -4,6 +4,7 @@ import io.github.xpakx.micro2.comment.CommentRepository;
 import io.github.xpakx.micro2.post.dto.PostDto;
 import io.github.xpakx.micro2.post.dto.PostRequest;
 import io.github.xpakx.micro2.post.error.PostNotFoundException;
+import io.github.xpakx.micro2.post.error.PostTooOldToEditException;
 import io.github.xpakx.micro2.tag.Tag;
 import io.github.xpakx.micro2.tag.TagService;
 import io.github.xpakx.micro2.user.UserAccount;
@@ -18,9 +19,11 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -164,5 +167,98 @@ class PostServiceTest {
                 PostNotFoundException.class,
                 () -> service.deletePost(1L, "username")
         );
+    }
+
+    @Test
+    void shouldThrowExceptionWhileUpdatingNonexistentPost() {
+        given(postRepository.findByIdAndUserUsername(anyLong(), anyString()))
+                .willReturn(Optional.empty());
+        injectMocks();
+
+        assertThrows(
+                PostNotFoundException.class,
+                () -> service.updatePost(getPostRequestWithContent("edited post"), 1L, "username")
+        );
+    }
+
+    @Test
+    void shouldNotUpdatePostOlderThan24h() {
+        given(postRepository.findByIdAndUserUsername(anyLong(), anyString()))
+                .willReturn(Optional.of(getOutdatedPost()));
+        injectMocks();
+
+        assertThrows(
+                PostTooOldToEditException.class,
+                () -> service.updatePost(getPostRequestWithContent("edited post"), 1L, "username")
+        );
+    }
+
+    private Post getOutdatedPost() {
+        Post result = new Post();
+        result.setUser(getUserWithUsername("username"));
+        result.setCreatedAt(LocalDateTime.now().minusDays(2));
+        return result;
+    }
+
+    private Post getNotOutdatedPost() {
+        Post result = new Post();
+        result.setUser(getUserWithUsername("username"));
+        result.setCreatedAt(LocalDateTime.now());
+        return result;
+    }
+
+    @Test
+    void shouldUpdatePost() {
+        given(postRepository.findByIdAndUserUsername(anyLong(), anyString()))
+                .willReturn(Optional.of(getNotOutdatedPost()));
+        given(postRepository.save(ArgumentMatchers.any(Post.class)))
+                .willReturn(getEmptyPost());
+        injectMocks();
+
+        service.updatePost(getPostRequestWithContent("update"), 1L,"username");
+
+        ArgumentCaptor<Post> postCaptor = ArgumentCaptor.forClass(Post.class);
+        then(postRepository)
+                .should(times(1))
+                .save(postCaptor.capture());
+        Post result = postCaptor.getValue();
+
+        assertNotNull(result);
+        assertThat(result.getUser().getUsername(), is("username"));
+        assertThat(result.getContent(), is("update"));
+        assertThat(result.isEdited(), is(true));
+    }
+
+    @Test
+    void shouldChangeTagsWhileUpdating() {
+        given(postRepository.findByIdAndUserUsername(anyLong(), anyString()))
+                .willReturn(Optional.of(getPostWithTag(List.of("tag", "tag2"))));
+        given(postRepository.save(ArgumentMatchers.any(Post.class)))
+                .willReturn(getEmptyPost());
+        given(tagService.addTags(anyString()))
+                .willReturn(Set.of(getTagWithName("newTag"), getTagWithName("newTag2")));
+        injectMocks();
+        injectMocks();
+
+        service.updatePost(getPostRequestWithContent("Post #newTag #newTag2"), 1L, "username");
+
+        ArgumentCaptor<Post> postCaptor = ArgumentCaptor.forClass(Post.class);
+        then(postRepository)
+                .should(times(1))
+                .save(postCaptor.capture());
+        Post result = postCaptor.getValue();
+
+        assertNotNull(result.getTags());
+        assertThat(result.getTags(), hasSize(2));
+        assertThat(result.getTags(), everyItem(either(hasProperty("name", is("newTag")))
+                .or(hasProperty("name", is("newTag2")))));
+    }
+
+    private Post getPostWithTag(List<String> tag) {
+        Post result = new Post();
+        result.setUser(getUserWithUsername("username"));
+        result.setCreatedAt(LocalDateTime.now());
+        result.setTags(tag.stream().map(this::getTagWithName).collect(Collectors.toSet()));
+        return result;
     }
 }
