e10ae06258e551d5c58bd01599f646f49f8dd2f5
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-23 14:43:08 +0200

    test(backend): add tests for getting tag lists

--- backend/src/test/java/io/github/xpakx/micro2/tag/TagServiceTest.java
+++ backend/src/test/java/io/github/xpakx/micro2/tag/TagServiceTest.java
@@ -1,16 +1,23 @@
 package io.github.xpakx.micro2.tag;
 
+import io.github.xpakx.micro2.tag.dto.TagDetails;
+import io.github.xpakx.micro2.tag.dto.TagName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
@@ -23,6 +30,8 @@ class TagServiceTest {
     private TagRepository tagRepository;
 
     private TagService service;
+
+    private final ProjectionFactory factory = new SpelAwareProxyProjectionFactory();
 
     @BeforeEach
     void setUp() {
@@ -147,5 +156,45 @@ class TagServiceTest {
         assertNotNull(result);
         assertThat(result, hasSize(15));
         assertThat(result, not(hasItem(hasProperty("name", is("tag16")))));
+    }
+
+    @Test
+    void shouldReturnTopTags() {
+        given(tagRepository.getTopTagsAfterDate(ArgumentMatchers.any(LocalDateTime.class)))
+                .willReturn(List.of(getTagDetails("tag1"), getTagDetails("tag2")));
+        injectMocks();
+
+        List<TagDetails> result = service.getTopTags();
+
+        assertNotNull(result);
+        assertThat(result, hasSize(2));
+        assertThat(result, hasItem(hasProperty("name", is("tag1"))));
+        assertThat(result, hasItem(hasProperty("name", is("tag2"))));
+    }
+
+    private TagDetails getTagDetails(String content) {
+        Tag tag = new Tag();
+        tag.setName(content);
+        return factory.createProjection(TagDetails.class, tag);
+    }
+
+    @Test
+    void shouldCompleteTagName() {
+        given(tagRepository.findFirst10ByNameStartsWith(anyString()))
+                .willReturn(List.of(getTagName("tag1"), getTagName("tag2")));
+        injectMocks();
+
+        List<TagName> result = service.autocomplete("ta");
+
+        assertNotNull(result);
+        assertThat(result, hasSize(2));
+        assertThat(result, hasItem(hasProperty("name", is("tag1"))));
+        assertThat(result, hasItem(hasProperty("name", is("tag2"))));
+    }
+
+    private TagName getTagName(String content) {
+        Tag tag = new Tag();
+        tag.setName(content);
+        return factory.createProjection(TagName.class, tag);
     }
 }
