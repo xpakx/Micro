a9dec64d96fc22d1f39e1e517b9c5188c8d50cd1
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-25 17:50:34 +0200

    test(backend): add tests for creating posts

--- backend/src/test/java/io/github/xpakx/micro2/post/PostControllerTest.java
+++ backend/src/test/java/io/github/xpakx/micro2/post/PostControllerTest.java
@@ -1,9 +1,13 @@
 package io.github.xpakx.micro2.post;
 
+import io.github.xpakx.micro2.post.dto.PostRequest;
 import io.github.xpakx.micro2.security.JwtTokenUtils;
+import io.github.xpakx.micro2.tag.Tag;
+import io.github.xpakx.micro2.tag.TagRepository;
 import io.github.xpakx.micro2.user.UserAccount;
 import io.github.xpakx.micro2.user.UserRepository;
 import io.github.xpakx.micro2.user.UserService;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,8 +16,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import java.util.HashSet;
+import java.util.Optional;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostControllerTest {
@@ -30,6 +39,8 @@ class PostControllerTest {
     UserService userService;
     @Autowired
     PostRepository postRepository;
+    @Autowired
+    TagRepository tagRepository;
 
     @BeforeEach
     void setUp() {
@@ -44,11 +55,68 @@ class PostControllerTest {
 
     @AfterEach
     void tearDown() {
+        postRepository.deleteAll();
+        tagRepository.deleteAll();
         userRepository.deleteAll();
     }
 
     private String tokenFor(String username) {
         return jwtTokenUtil.generateToken(userService.loadUserByUsername(username));
+    }
+
+    @Test
+    void shouldRespondWith401ToAddPostIfUserUnauthorized() {
+        given()
+                .log()
+                .uri()
+        .when()
+                .post(baseUrl + "/user/{username}/post", "user1")
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldAddNewPost() {
+        PostRequest request = getValidPostRequest("content");
+        given()
+                .log()
+                .uri().auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/user/{username}/post", "user1")
+        .then()
+                .statusCode(CREATED.value())
+                .body("message", equalTo(request.getMessage()))
+                .body("username", equalTo("user1"))
+                .body("likeCount", equalTo(0))
+                .body("dislikeCount", equalTo(0));
+    }
+
+    private PostRequest getValidPostRequest(String content) {
+        PostRequest request = new PostRequest();
+        request.setMessage(content);
+        return request;
+    }
+
+    @Test
+    void shouldAddNewPostWithTag() {
+        PostRequest request = getValidPostRequest("content #tag");
+        given()
+                .log()
+                .uri().auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/user/{username}/post", "user1")
+        .then()
+                .statusCode(CREATED.value())
+                .body("message", equalTo(request.getMessage()));
+
+        Optional<Tag> tagInDb = tagRepository.findByName("tag");
+        assertTrue(tagInDb.isPresent());
     }
 
 }
