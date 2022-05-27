58d053c54cc9a4f3038bcd92f7c3852d710dbab2
xpakx <55586470+xpakx@users.noreply.github.com>
2022-05-27 13:03:29 +0200

    test(backend): add tests for adding comment

---
+++ backend/src/test/java/io/github/xpakx/micro2/comment/CommentControllerTest.java
@@ -0,0 +1,127 @@
+package io.github.xpakx.micro2.comment;
+
+import io.github.xpakx.micro2.comment.dto.CommentRequest;
+import io.github.xpakx.micro2.post.Post;
+import io.github.xpakx.micro2.post.PostRepository;
+import io.github.xpakx.micro2.post.dto.PostRequest;
+import io.github.xpakx.micro2.security.JwtTokenUtils;
+import io.github.xpakx.micro2.user.UserAccount;
+import io.github.xpakx.micro2.user.UserRepository;
+import io.github.xpakx.micro2.user.UserService;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpStatus.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CommentControllerTest {
+    @LocalServerPort
+    private int port;
+    private String baseUrl;
+    private Long userId;
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    JwtTokenUtils jwtTokenUtil;
+    @Autowired
+    UserService userService;
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = "http://localhost".concat(":").concat(port + "");
+        UserAccount user = new UserAccount();
+        user.setUsername("user1");
+        user.setPassword("password");
+        user.setRoles(new HashSet<>());
+        user = userRepository.save(user);
+        this.userId = user.getId();
+    }
+
+    @AfterEach
+    void tearDown() {
+        commentRepository.deleteAll();
+        postRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    private String tokenFor(String username) {
+        return jwtTokenUtil.generateToken(userService.loadUserByUsername(username));
+    }
+
+    @Test
+    void shouldRespondWith401ToAddCommentIfUserUnauthorized() {
+        given()
+                .log()
+                .uri()
+        .when()
+                .post(baseUrl + "/user/{username}/post/{postId}/comments", "user1", 1)
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldNotAddNewCommentToNonexistentPost() {
+        CommentRequest request = getValidCommentRequest("content");
+        given()
+                .log()
+                .uri().auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/user/{username}/post/{postId}/comments", "user1", 1)
+        .then()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    @Test
+    void shouldAddNewComment() {
+        CommentRequest request = getValidCommentRequest("content");
+        Long postId = addPostAndReturnId();
+        given()
+                .log()
+                .uri().auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/user/{username}/post/{postId}/comments", "user1", postId)
+        .then()
+                .statusCode(CREATED.value())
+                .body("message", equalTo(request.getMessage()))
+                .body("username", equalTo("user1"))
+                .body("likeCount", equalTo(0))
+                .body("dislikeCount", equalTo(0));
+    }
+
+    private CommentRequest getValidCommentRequest(String content) {
+        CommentRequest request = new CommentRequest();
+        request.setMessage(content);
+        return request;
+    }
+    private Long addPostAndReturnId() {
+        Post post = new Post();
+        post.setContent("content");
+        post.setLikeCount(0);
+        post.setDislikeCount(0);
+        post.setUser(userRepository.getById(userId));
+        post.setCreatedAt(LocalDateTime.now());
+        return postRepository.save(post).getId();
+    }
+}
