38b412a120fcabe4f5636387b7899a58a11042de
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-08 14:17:35 +0200

    feat(backend): add service method for following tags

--- backend/src/main/java/io/github/xpakx/micro2/follows/FollowService.java
+++ backend/src/main/java/io/github/xpakx/micro2/follows/FollowService.java
@@ -1,5 +1,6 @@
 package io.github.xpakx.micro2.follows;
 
+import io.github.xpakx.micro2.tag.TagRepository;
 import io.github.xpakx.micro2.user.UserRepository;
 import io.github.xpakx.micro2.user.error.UserNotFoundException;
 import lombok.AllArgsConstructor;
@@ -13,10 +14,17 @@ public class FollowService {
     public final FollowsRepository followsRepository;
     public final FollowPostRepository postRepository;
     public final UserRepository userRepository;
+    public final TagRepository tagRepository;
 
     public void followUser(String byUsername, String username) {
-        UserFollows follows = followsRepository.findWithUsers(byUsername).orElse(getNewFollowObject(username));
+        UserFollows follows = followsRepository.findWithUsers(byUsername).orElse(getNewFollowObject(byUsername));
         follows.getUsers().add(userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new));
+        followsRepository.save(follows);
+    }
+
+    public void followTag(String byUsername, String tagName) {
+        UserFollows follows = followsRepository.findWIthTags(byUsername).orElse(getNewFollowObject(byUsername));
+        follows.getTags().add(tagRepository.findByName(tagName).orElseThrow());
         followsRepository.save(follows);
     }
 
