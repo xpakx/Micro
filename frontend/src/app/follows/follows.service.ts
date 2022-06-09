5d317d09816bc470da576f33e1662d0846773bf0
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-09 14:09:27 +0200

    feat(frontend): add service for follows

---
+++ frontend/src/app/follows/follows.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { FollowRequest } from './dto/follow-request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FollowsService {
+  private apiServerUrl = environment.apiServerUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public followUser(request: FollowRequest):  Observable<any> {
+    return this.http.post<any>(`${this.apiServerUrl}/follows/users`, request);
+  }
+
+  public unfollowUser(name: String):  Observable<any> {
+    return this.http.delete<any>(`${this.apiServerUrl}/follows/users/${name}`);
+  }
+
+  public followtag(request: FollowRequest):  Observable<any> {
+    return this.http.post<any>(`${this.apiServerUrl}/follows/tags`, request);
+  }
+
+  public unfollowTag(name: String):  Observable<any> {
+    return this.http.delete<any>(`${this.apiServerUrl}/follows/tags/${name}`);
+  }
+}
