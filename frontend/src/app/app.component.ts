5af4e3b18bbcdadb25068122bcc1cc4ce1c9d48d
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-12 14:53:43 +0200

    feat(frontend): show mention count to user

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faBell, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MentionCount } from './mention/dto/mention-count';
+import { MentionService } from './mention/mention.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'micro2';
   faSearch = faSearch;
   faNotif = faBell;
+  mentionsCount: number = 0;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private mentionService: MentionService) { }
+
+  ngOnInit(): void {
+    if(localStorage.getItem("token")) {
+      this.mentionService.getMentionsCount().subscribe({
+        next: (response: MentionCount) => this.updateMentions(response.count)
+      });
+    }
+  }
+
+  updateMentions(count: number): void {
+    this.mentionsCount = count;
+  }
 
   toMain() {
     this.router.navigate([''])
