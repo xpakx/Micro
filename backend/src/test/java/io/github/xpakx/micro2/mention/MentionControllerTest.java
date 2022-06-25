0627f677a42e6b849254280799a9d25d692d98bb
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-25 13:49:59 +0200

    test(backend): test getting mentions list

--- backend/src/test/java/io/github/xpakx/micro2/mention/MentionControllerTest.java
+++ backend/src/test/java/io/github/xpakx/micro2/mention/MentionControllerTest.java
@@ -1,5 +1,6 @@
 package io.github.xpakx.micro2.mention;
 
+import io.github.xpakx.micro2.comment.Comment;
 import io.github.xpakx.micro2.comment.CommentRepository;
 import io.github.xpakx.micro2.mention.dto.MentionReadRequest;
 import io.github.xpakx.micro2.post.Post;
@@ -45,6 +46,8 @@ class MentionControllerTest {
     PostRepository postRepository;
     @Autowired
     MentionRepository mentionRepository;
+    @Autowired
+    CommentRepository commentRepository;
 
     @BeforeEach
     void setUp() {
@@ -60,6 +63,7 @@ class MentionControllerTest {
     @AfterEach
     void tearDown() {
         mentionRepository.deleteAll();
+        commentRepository.deleteAll();
         postRepository.deleteAll();
         userRepository.deleteAll();
     }
@@ -209,5 +213,87 @@ class MentionControllerTest {
         List<Mention> mentions = Streams.stream(mentionRepository.findAll()).collect(Collectors.toList());
         assertThat(mentions, hasSize(3));
         assertThat(mentions, everyItem(hasProperty("read", equalTo(true))));
+    }
+
+    @Test
+    void shouldRespondWith401ToGetMentionsRequestIfUserUnauthorized() {
+        given()
+                .log()
+                .uri()
+        .when()
+                .get(baseUrl + "/mentions")
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWithMentions() {
+        addUser("user2");
+        Long post1Id = addPostAndReturnId("post1", "user2");
+        Long post2Id = addPostAndReturnId("post2", "user2");
+        Long commentId = addCommentAndReturnId("post2", "user2", post1Id);
+        addMentionForPost(post1Id);
+        addMentionForPost(post2Id);
+        addMentionForComment(commentId, post1Id);
+        given()
+                .log()
+                .uri()
+                .auth()
+                .oauth2(tokenFor("user1"))
+        .when()
+                .get(baseUrl + "/mentions")
+        .then()
+                .statusCode(OK.value())
+                .body("content", hasSize(3));
+    }
+
+    private void addUser(String username) {
+        UserAccount user = new UserAccount();
+        user.setUsername(username);
+        user.setPassword("password");
+        user.setRoles(new HashSet<>());
+        userRepository.save(user);
+    }
+
+    private Long addPostAndReturnId(String content, String username) {
+        Post post = new Post();
+        post.setLikeCount(0);
+        post.setDislikeCount(0);
+        post.setCreatedAt(LocalDateTime.now());
+        post.setContent(content);
+        post.setUser(userRepository.findByUsername(username).get());
+        return postRepository.save(post).getId();
+    }
+
+    private Long addCommentAndReturnId(String content, String username, Long postId) {
+        Comment comment = new Comment();
+        comment.setLikeCount(0);
+        comment.setDislikeCount(0);
+        comment.setCreatedAt(LocalDateTime.now());
+        comment.setContent(content);
+        comment.setUser(userRepository.findByUsername(username).get());
+        comment.setPost(postRepository.findById(postId).get());
+        return commentRepository.save(comment).getId();
+    }
+
+    private void addMentionForPost(Long postId) {
+        UserAccount user = userRepository.findByUsername("user1").get();
+        Mention mention = new Mention();
+        mention.setRead(false);
+        mention.setPost(postRepository.findById(postId).get());
+        mention.setAuthor(user);
+        mention.setMentioned(user);
+        mentionRepository.save(mention);
+    }
+
+    private void addMentionForComment(Long commentId, Long postId) {
+        UserAccount user = userRepository.findByUsername("user1").get();
+        Mention mention = new Mention();
+        mention.setRead(false);
+        mention.setPost(postRepository.findById(postId).get());
+        mention.setComment(commentRepository.findById(commentId).get());
+        mention.setAuthor(user);
+        mention.setMentioned(user);
+        mentionRepository.save(mention);
     }
 }
