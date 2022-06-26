3996efafa07b2baef9a16f9f0e5d90d21fed49bc
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-26 21:54:30 +0200

    feat(backend): add roles to JWT token

--- backend/src/main/java/io/github/xpakx/micro2/security/JwtTokenUtils.java
+++ backend/src/main/java/io/github/xpakx/micro2/security/JwtTokenUtils.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenUtils {
@@ -50,11 +52,17 @@ public class JwtTokenUtils {
     }
 
     public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
+        Claims claims = Jwts.claims().setSubject(userDetails.getUsername());
+        claims.put(
+                "roles",
+                userDetails.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList())
+        );
         return doGenerateToken(claims, userDetails.getUsername());
     }
 
-    private String doGenerateToken(Map<String, Object> claims, String subject) {
+    private String doGenerateToken(Claims claims, String subject) {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
