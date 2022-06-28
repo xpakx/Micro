61d04f14064898d3a2cfbf9bc7cf99bb238614da
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-28 14:30:55 +0200

    feat(frontend): add functionalities to pagination component

--- frontend/src/app/post/pagination/pagination.component.ts
+++ frontend/src/app/post/pagination/pagination.component.ts
@@ -8,16 +8,26 @@ import { Pageable } from 'src/app/common/dto/pageable';
 })
 export class PaginationComponent implements OnInit {
   @Output("page") pageEvent = new EventEmitter<number>();
-  @Input("pageData") pageable?: Pageable;
+  @Input("pages") totalPages: number = 0;
+  @Input("curr") currentPage: number = 0;
+  @Input("last") lastPage: boolean = true;
+  @Input("first") firstPage: boolean = true;
   pages: number[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.pages = [0,1, 2, 3];
+    let min: number = Math.max(0, this.currentPage-2);
+    let max: number = Math.min(this.totalPages, this.currentPage+3);
+    for(let i: number = min; i<max; i++) {
+      this.pages.push(i);
+    }
   }
 
-  toPage(page: number) {
+  toPage(page: number): void {
+    if((this.lastPage && page >= this.currentPage) || (this.firstPage && page <= this.currentPage)) {
+      return;
+    }
     this.pageEvent.emit(page);
   }
 
