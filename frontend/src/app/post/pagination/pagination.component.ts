92412fa2ecd8de41a5a8c0a455feb1e649703644
xpakx <55586470+xpakx@users.noreply.github.com>
2022-06-30 10:55:29 +0200

    fix(frontend): fix refreshing pagination after page change

--- frontend/src/app/post/pagination/pagination.component.ts
+++ frontend/src/app/post/pagination/pagination.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Pageable } from 'src/app/common/dto/pageable';
 
 @Component({
   selector: 'app-pagination',
@@ -9,17 +8,28 @@ import { Pageable } from 'src/app/common/dto/pageable';
 export class PaginationComponent implements OnInit {
   @Output("page") pageEvent = new EventEmitter<number>();
   @Input("pages") totalPages: number = 0;
-  @Input("curr") currentPage: number = 0;
+  currentPage: number = 0;
   @Input("last") lastPage: boolean = true;
   @Input("first") firstPage: boolean = true;
   pages: number[] = [];
 
   constructor() { }
 
+  
+  @Input("curr") set current(page: number) {
+    this.currentPage = page;
+    this.calculatePages();
+  } 
+
   ngOnInit(): void {
-    let min: number = Math.max(0, this.currentPage-2);
-    let max: number = Math.min(this.totalPages, this.currentPage+3);
-    for(let i: number = min; i<max; i++) {
+  
+  }
+
+  private calculatePages() {
+    this.pages = [];
+    let min: number = Math.max(0, this.currentPage - 2);
+    let max: number = Math.min(this.totalPages, this.currentPage + 3);
+    for (let i: number = min; i < max; i++) {
       this.pages.push(i);
     }
   }
