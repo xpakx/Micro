7dff99aa2f97ec8a9d9f4672494990b9d9e9d930
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-04 11:04:59 +0200

    fix(backend): send data in Json for PostDto in correct format

--- backend/src/main/java/io/github/xpakx/micro2/post/dto/PostDto.java
+++ backend/src/main/java/io/github/xpakx/micro2/post/dto/PostDto.java
@@ -1,5 +1,8 @@
 package io.github.xpakx.micro2.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import io.github.xpakx.micro2.post.Post;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +15,8 @@ public class PostDto {
     private Long id;
     private String message;
     private String username;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss")
     private LocalDateTime createdAt;
     private Integer likeCount;
     private Integer dislikeCount;
