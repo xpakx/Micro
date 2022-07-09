ee301ab64f769c627d70e762d1f163bd227439e9
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-09 12:06:35 +0200

    feat(frontend/ui): let user open search input

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faBell, faEnvelope, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { MentionCount } from './mention/dto/mention-count';
@@ -18,8 +19,15 @@ export class AppComponent implements OnInit {
   faMessage = faEnvelope;
   mentionsCount: number = 0;
   messagesCount: number = 0;
+  searchForm: FormGroup;
+  smallWindow: boolean = false;
+  showSearch: boolean = false;
 
-  constructor(private router: Router, private mentionService: MentionService, private messageService: MessageService) { }
+  constructor(private router: Router, private mentionService: MentionService, private messageService: MessageService, private fb: FormBuilder) { 
+    this.searchForm = this.fb.group({
+      search: ['']
+    });
+  }
 
   get logged(): boolean {
     if(localStorage.getItem("token")) {
@@ -68,5 +76,13 @@ export class AppComponent implements OnInit {
 
   toLogin() {
     this.router.navigate(['login']);
+  }
+
+  search(): void {
+    if(!this.showSearch) {
+      this.showSearch = true;
+    } else {
+      this.router.navigate(['/search'], { queryParams: {search: this.searchForm.controls['search'].value}});
+    }
   }
 }
