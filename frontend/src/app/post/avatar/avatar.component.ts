94537f00778b3e3a503dc4e6b3bf7112b3835a0d
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-15 12:42:27 +0200

    chore(frontend): use local avatar if no avatar set for user

--- frontend/src/app/post/avatar/avatar.component.ts
+++ frontend/src/app/post/avatar/avatar.component.ts
@@ -6,10 +6,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent implements OnInit {
-  @Input('url') url: String = 'https://loganfuneralchapel.com/wp-content/uploads/2017/01/xgeneric-profile-avatar_352864.jpg.pagespeed.ic.tKiXzscPKc.jpg';
+  @Input('url') url: String = '/assets/default-avatar.jpg';
+  
   color: String = 'gray';
   @Input('gender') set gender(value: String) {
-    console.log(value)
     if(value == "male") {
       this.color = 'seagreen';
     } else if(value == "female") {
@@ -19,7 +19,6 @@ export class AvatarComponent implements OnInit {
     } else {
       this.color = 'gold';
     }
-    console.log(this.color)
   } 
 
   constructor() { }
