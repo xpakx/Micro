86f28eea4083927aa658e0b45a07203cdfaa5b38
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-15 12:29:40 +0200

    feat(backend): restrict uploaded files size

---
+++ backend/src/main/java/io/github/xpakx/micro2/config/FileUploadConfig.java
@@ -0,0 +1,16 @@
+package io.github.xpakx.micro2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class FileUploadConfig {
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(5242880);
+        return multipartResolver;
+    }
+}
