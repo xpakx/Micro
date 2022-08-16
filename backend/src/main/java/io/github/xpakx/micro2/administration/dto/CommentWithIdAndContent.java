bbd50386003a04cead746f8d3bd8790676f7723c
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-16 14:30:08 +0200

    fix(backend): add post id to comment data

--- backend/src/main/java/io/github/xpakx/micro2/administration/dto/CommentWithIdAndContent.java
+++ backend/src/main/java/io/github/xpakx/micro2/administration/dto/CommentWithIdAndContent.java
@@ -1,6 +1,9 @@
 package io.github.xpakx.micro2.administration.dto;
 
+import io.github.xpakx.micro2.post.dto.PostWithOnlyId;
+
 public interface CommentWithIdAndContent {
     Long getId();
     String getContent();
+    PostWithOnlyId getPost();
 }
